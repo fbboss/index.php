6f05ae67160982f0e2bbe2cf0966a078278c37be
fbboss <50343438+fbboss@users.noreply.github.com>
2019-05-07 04:34:50 +0600

    Update react.php

--- react.php
+++ react.php
@@ -20,7 +20,7 @@ for($i=0;$i<sizeof($file_array);$i++)
 		echo "<br/>";
         for($d=1;$d<=count($puaru[data]);$d++){
         set_time_limit(0);
-        $camxuc= array('LIKE','LIKE');
+        $camxuc= array('LOVE','');
         $mess=$camxuc[rand(0,count($camxuc)-1)];
         echo puaru('https://graph.facebook.com/'.$puaru[data][$d-1][id].'/reactions?type='.$mess.'&method=post&access_token='.$token[0].'');       
 }	    
